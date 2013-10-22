a4f71530a3d22c0c2acff16c13962808e7b9db84
doan vinh <vinh.doan@sjsu.edu>
2013-10-22 12:35:38 -0700

    panorama

---
+++ CineQuest/CineQuest/FilmDetailsPanorama.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Microsoft.Phone.Tasks;
+namespace CineQuest
+{
+    public partial class FilmDetailsPanorama : PhoneApplicationPage
+    {
+        public FilmDetailsPanorama()
+        {
+            InitializeComponent();
+        }
+
+        /* When page is navigated to set data context to selected item in list */
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            string selectedIndex = "";
+            if (NavigationContext.QueryString.TryGetValue("selectedItem", out selectedIndex))
+            {
+                int index = 0;// int.Parse(selectedIndex); //for testing throws error need to look into it
+                DataContext = App.ViewModel.Items[index];
+                //System.Diagnostics.Debug.WriteLine("test"+DataContext);
+
+            }
+        }
+
+        private void addSchedule(object sender, RoutedEventArgs e)
+        {
+            SaveAppointmentTask saveAppointmentTask = new SaveAppointmentTask();
+            //String test = ListTitle.Text;
+            saveAppointmentTask.StartTime = DateTime.Now.AddHours(2);
+            saveAppointmentTask.EndTime = DateTime.Now.AddHours(3); // == startTime + duration;
+            saveAppointmentTask.Subject = (String)ListTitle.Title ;
+            saveAppointmentTask.Location = "CineQuest";
+            saveAppointmentTask.Details = "Appointment details";
+            saveAppointmentTask.IsAllDayEvent = false;
+            saveAppointmentTask.Reminder = Reminder.FifteenMinutes;
+            saveAppointmentTask.AppointmentStatus = Microsoft.Phone.UserData.AppointmentStatus.Busy;
+            saveAppointmentTask.Show();
+        }
+    }
+}
